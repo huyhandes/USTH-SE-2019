ca8daf409f3456e59b3b8c01afadce7a0c875eb3
Shay <huybq.b8072@st.usth.edu.vn>
2019-04-01 22:25:07 +0700

    new design upload

--- Group6/routes/index.js
+++ Group6/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Main page' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login page',layout: false });
 });
 
 module.exports = router;
